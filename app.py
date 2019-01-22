c37966a1b7cfc6a0fe717c45a9e73b286ea2710c
CJE5SZH <adiachan@outlook.com>
2019-01-22 21:32:25 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+items = []
+
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # for item in items:
+        #     if item['name'] == name:
+        #         return item
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': f'An item with name {name} already exists'}
+        data = request.get_json(silent=True)
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
