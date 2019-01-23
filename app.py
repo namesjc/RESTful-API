8683b249f1fe294afa4e19585ff3ebefa3704e2d
CJE5SZH <adiachan@outlook.com>
2019-01-23 11:26:44 +0800

    optimise request parser

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ jwt = JWT(app, authenticate, identity)  #/auth
 items = []
 
 class Item(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help='This field cannot be left blank'
+    )
+
     @jwt_required()
     def get(self, name):
         item = next(filter(lambda x: x['name'] == name, items), None)
@@ -20,10 +28,10 @@ class Item(Resource):
         #         return item
         return {'item': item}, 200 if item else 404
 
-    def post(self, name):
+    def post(self, name):      
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': f'An item with name {name} already exists'}, 400
-        data = request.get_json(silent=True)
+        data = Item.parser.parse_args()
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
@@ -33,14 +41,8 @@ class Item(Resource):
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted!'}
 
-    def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-            type=float,
-            required=True,
-            help='This field cannot be left blank'
-        )
-        data = parser.parse_args()
+    def put(self, name):      
+        data = Item.parser.parse_args()
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
