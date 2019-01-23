1f6ecdeec6c5042cd0e737abd40855c6e80787b1
CJE5SZH <adiachan@outlook.com>
2019-01-23 14:49:31 +0800

    register user

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
+
 from security import authenticate, identity
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key = 'echo'
@@ -58,6 +60,7 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 
 if __name__ == '__main__':
